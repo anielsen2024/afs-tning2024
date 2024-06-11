380fd9f6fffa4df55c6b2c3c9ad73902130efe5e
anielsen2024 <anni10@videndjurs.net>
2024-06-11 15:24:11 +0200

    Update main.py

--- streamlit-web/main.py
+++ streamlit-web/main.py
@@ -22,7 +22,7 @@ def get_pdf_page_count(pdf_path):
         return len(pdf.pages)
 
 # Angiv PDF-stien
-pdf_path = "C:/Users/andly/OneDrive/Desktop_Lenovo/Dokumentationsopgave/Dokumentationsopgave.pdf"
+pdf_path = "Dokumentationsopgave.pdf"
 
 # Hent antallet af sider i PDF'en
 page_count = get_pdf_page_count(pdf_path)
